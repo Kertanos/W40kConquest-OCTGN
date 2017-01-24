2c50054f4a0944c56df5e15bf50b6046f34400f3
Troy Nordine <tdnordine@gmail.com>
2017-01-24 09:02:58 -0600

    Update local build system
    Update to fit in with my local build approach.

--- af04f855-58c4-4db3-a191-45fe33381679/scripts/actions.py
+++ af04f855-58c4-4db3-a191-45fe33381679/scripts/actions.py
@@ -168,6 +168,24 @@ def createToken(group, x = 0, y = 0):
 			X1-=50
 	notify("{} creates {} {} Token(s)".format(me,quantity,cards))
 
+def createSynapse(group, x = 0, y = 0):
+	mute()	
+	guid,quantity=askCard({'Type':'Synapse'})
+	if guid == None: return
+	if me.isInverted: 
+		X1=-150
+		for i in range(quantity):
+			cards=table.create(guid, X1, -288)
+			X1+=50	
+	else:
+		X1=150
+		for i in range(quantity):
+			cards=table.create(guid, X1, 200)
+			X1-=50
+	notify("{} creates {} Token(s)".format(me,cards))
+
+
+
 def ServoSkull(group, x=0,y=0):
 	mute()
 	servo= (card for card in table if card.controller == me and card.Type == "Skull")
